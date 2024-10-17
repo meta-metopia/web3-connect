642156460a35f1eaa3dd2534a0e114e481951a0a
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-10-17 11:53:34 +0800

    feat: add ci support (#1)
    * feat: add publish ci
    
    v0.0.12
    
    * v0.0.13
    
    * fix: remove leading v

---
+++ scripts/publish.js
@@ -0,0 +1,28 @@
+const execSync = require("child_process").execSync;
+
+// get latest version from command line
+let version = process.argv[2];
+// remove any leading 'v'
+version = version.replace(/^v/, "");
+
+// update package.json
+const fs = require("fs");
+const packageJsonFile = JSON.parse(fs.readFileSync("package.json"));
+packageJsonFile.version = version;
+
+fs.writeFileSync("package.json", JSON.stringify(packageJsonFile, null, 2));
+
+// git add, and commit
+try {
+  execSync("git add .");
+  execSync(`git commit -m "v${version}"`);
+} catch (e) {
+  console.log("No changes to commit");
+}
+
+// use NPM_TOKEN to authenticate
+execSync(
+  `npm config set //registry.npmjs.org/:_authToken ${process.env.NPM_TOKEN}`,
+);
+// publish to npm
+execSync("npm publish");
