175654844b0a80ee9e29018f90129d8dabc05844
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-02-06 00:07:18 +0900

    fix: wallet connect provider now exported (#30)

--- examples/next-js-example/scripts/deploy-preview.sh
+++ examples/next-js-example/scripts/deploy-preview.sh
@@ -1,3 +1,3 @@
 pnpm i && pnpm vercel pull --yes --token=$VERCEL_TOKEN && pnpm vercel build
 URL="$(pnpm vercel --prebuilt --token=$VERCEL_TOKEN --scope=$SCOPE)"
-vercel alias set "$URL" $PREVIEW_URL --scope=$SCOPE --token=$VERCEL_TOKEN
+pnpm vercel alias set "$URL" $PREVIEW_URL --scope=$SCOPE --token=$VERCEL_TOKEN
